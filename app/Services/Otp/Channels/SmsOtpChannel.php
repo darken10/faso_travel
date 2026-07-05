fee01a76444dd50b3817ea872c372b124efbd83d
Inoussa <inoussa.zerbo@africasys.com>
2026-07-05 11:14:15 +0000

    feat(sms): implement SMS sending functionality with support for multiple drivers

--- app/Services/Otp/Channels/SmsOtpChannel.php
+++ app/Services/Otp/Channels/SmsOtpChannel.php
@@ -4,6 +4,7 @@ namespace App\Services\Otp\Channels;
 
 use App\Enums\OtpChannelType;
 use App\Models\User;
+use Illuminate\Support\Facades\Log;
 use Twilio\Rest\Client as TwilioClient;
 
 class SmsOtpChannel extends AbstractOtpChannel
@@ -25,6 +26,22 @@ class SmsOtpChannel extends AbstractOtpChannel
 
     public function send(User $user, string $otp): void
     {
+        $driver = config('sms.driver', 'twillo');
+        Switch ($driver) {
+            case 'twillo':
+                $this->sendWithTwillio($user, $otp);
+                break;
+            case 'textbee':
+                $this->sendWithTextbee($user, $otp);
+                break;
+            default:
+                throw new \RuntimeException("Driver SMS inconnu : {$driver}");
+        }
+        
+    }
+
+    private function sendWithTwillio(User $user, string $otp): void
+    {
         $sid   = config('sms.twillo.twilio_sid');
         $token = config('sms.twillo.twilio_token');
         $from  = config('sms.twillo.twilio_phone_number');
@@ -38,5 +55,38 @@ class SmsOtpChannel extends AbstractOtpChannel
             'from' => $from,
             'body' => "Votre code LIPTRA : {$otp}. Valable 10 minutes.",
         ]);
+    }
+
+    private function sendWithTextbee(User $user, string $otp): void
+    {
+        $apiKey = config('sms.textbee.textbee_api_key');
+        $device_id = config('sms.textbee.textbee_device_id');
+        $apiBaseUrl = config('sms.textbee.textbee_api_url');
+
+        if (!$apiKey || !$device_id || !$apiBaseUrl) {
+            Log::error('Configuration SMS Textbee manquante. Définissez TEXTBEE_API_KEY, TEXTBEE_DEVICE_ID, TEXTBEE_API_URL dans .env');
+            throw new \RuntimeException('Configuration SMS Textbee manquante. Définissez TEXTBEE_API_KEY, TEXTBEE_DEVICE_ID, TEXTBEE_API_URL dans .env');
+        }
+
+        $url = $apiBaseUrl . "/gateway/devices/" . $device_id . "/send-sms";
+        $response = \Http::withHeaders([
+            'x-api-key' => $apiKey,
+            'Content-Type' => 'application/json',
+        ])->post($url, [
+            'recipients' => [$this->fullPhone($user)],
+            'message' => "Votre code  de verification : {$otp}. Valable 10 minutes. Merci de ne pas partager ce code avec qui que ce soit.",
+        ]
+        );
+
+        if (!$response->successful()) {
+            Log::error('Erreur lors de l\'envoi du SMS via Textbee : ' . $response->body());
+            throw new \RuntimeException('Erreur lors de l\'envoi du SMS via Textbee : ' . $response->body());
+        }
+
+        Log::info('SMS envoyé avec succès via Textbee à ' . $this->fullPhone($user));
+        Log::info('Response : ' . $response->body());
+
+
+
     }
 }
--- config/sms.php
+++ config/sms.php
@@ -1,10 +1,17 @@
 <?php
 
 return [
+    'driver' => env('SMS_DRIVER', 'textbee'),
+    
     'twillo' =>[
         "twilio_sid" => env('TWILIO_SID'),
         "twilio_token" => env('TWILIO_TOKEN'),
         "twilio_phone_number" => env('TWILIO_PHONE_NUMBER'),
         "twilio_whatsapp_from" => env('TWILIO_WHATSAPP_FROM'),
     ],
+    'textbee' => [
+        "textbee_api_key" => env('TEXTBEE_API_KEY'),
+        "textbee_device_id" => env('TEXTBEE_DEVICE_ID'),
+        'textbee_api_url' => env('TEXTBEE_API_URL'),
+    ]
 ];
